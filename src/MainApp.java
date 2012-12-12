5e6aff2446b0b750cd4ab116fcfdbd09ed6c5223
Yogesh Nagarur <yogeshnagarur@gmail.com>
2012-12-12 17:00:47 +0000

    some extra class that works

--- src/MainApp.java
+++ src/MainApp.java
@@ -13,23 +13,25 @@ public class MainApp {
 	private static RangerInterface sonar = null;
 	private static PlayerClient robot = null;
 	private static double[] sonarValues;
+	private static double x;
+	private static double y;
 
 	public static void main(String[] args) {
 
 		if (args.length == 0) {
 			System.out.println("R04749");
 		} else {
-			robotMovementToTarget(Double.parseDouble(args[0]),
-					Double.parseDouble(args[1]));
+			connectToRobot();
+			x = Double.parseDouble(args[0]);
+			y = Double.parseDouble(args[1]);
+			robotMovementToTarget(x, y);
 		}
 	}
 
 	protected static void robotMovementToTarget(final double x, final double y) {
-		connectToRobot();
-		collectionThread();
-
 		PlayerPose2d goTo = new PlayerPose2d(x, y, 0);
 		pos2D.setPosition(goTo, new PlayerPose2d(), 0);
+		collectionThread(x, y);
 
 	}
 
@@ -48,21 +50,33 @@ public class MainApp {
 		robot.runThreaded(-1, -1);
 	}
 
-	private static void collectionThread() {
+	private static void collectionThread(final double x, final double y) {
 		Thread collection = new Thread() {
-			double wallDistance = 1;
+
 			public void run() {
 				while (true) {
-
-					if (sonar.isDataReady()) {
-						sonarValues = sonar.getData().getRanges();
-						System.out.println(sonarValues[0]);
-						if(sonarValues[0] < wallDistance){
-							pos2D.setSpeed(0, 0);
+					while (!sonar.isDataReady())
+						;
+					sonarValues = sonar.getData().getRanges();
+					if (isClose(sonarValues)) {
+						switch (checkDirection(sonarValues)) {
+						case 0:
+							turnLeft(1, sonarValues);
+							break;
+						case 1:
+							turnLeft(-1, sonarValues);
+							break;
+						case 2:
+							turnRight(1);
+							break;
+						case 3:
+							turnRight(-1);
+							break;
 						}
+						robotMovementToTarget(x, y);
 					}
 					try {
-						sleep(5);
+						sleep(200);
 					} catch (InterruptedException e) {
 					}
 				}
@@ -71,8 +85,54 @@ public class MainApp {
 		collection.start();
 	}
 
-	private static double roundTwoDecimals(double d) {
-		DecimalFormat twoDForm = new DecimalFormat("#.##");
-		return Double.valueOf(twoDForm.format(d));
+	protected static int checkDirection(double[] sonarValues2) {
+		if (sonarValues2[0] < sonarValues2[1]) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+
+	protected static void turnRight(int i) {
+
+	}
+
+	protected static void turnLeft(int i, double[] sonarValues2) {
+		if (i == 1) {
+			pos2D.setSpeed(0, 0.5);
+			try {
+				Thread.sleep(3142);
+			} catch (InterruptedException e) {
+
+			}
+		} else if (i == -1) {
+			pos2D.setSpeed(0, -0.5);
+			try {
+				Thread.sleep(3142);
+			} catch (InterruptedException e) {
+			}
+		}
+		goForwardABit(sonarValues2);
+
+	}
+
+	private static void goForwardABit(double[] sonarValues2) {
+
+		while (!(sonarValues2[6] > 7 || sonarValues2[7] > 7))
+		{
+			pos2D.setSpeed(1, 0);
+		}
+
+		robotMovementToTarget(x, y);
+
+	}
+
+	private static boolean isClose(double[] sonarValues2) {
+		double distanceAvoid = 0.5;
+		if (sonarValues2[0] < distanceAvoid || sonarValues2[1] < distanceAvoid) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
---
+++ src/MainApp2.java
@@ -0,0 +1,304 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javaclient3.PlayerClient;
+import javaclient3.PlayerException;
+import javaclient3.Position2DInterface;
+import javaclient3.RangerInterface;
+import javaclient3.structures.PlayerConstants;
+import javaclient3.structures.PlayerPose2d;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainApp2 {
+
+	Robot robot;
+	double x, y;
+	PlayerPose2d p;
+
+	public MainApp2() {
+		System.out.println("R02823");
+	}
+
+	public MainApp2(String[] args) {
+		if (args.length == 0) {
+			System.out.println("R02823");
+		}
+		if (args.length == 2) {
+			robot = new Robot();
+			x = Double.parseDouble(args[0]);
+			y = Double.parseDouble(args[1]);
+			// //////////////////////////////////////////////
+			for (int i = 0; i < args.length; i++) { // //
+				System.out.println(args[i]); // //
+			} // //
+			System.out.println("x : " + x); // //
+			System.out.println("y : " + y); // //
+			// //////////////////////////////////////////////
+			goToXY();
+			initThread();
+		}
+
+		if (args.length == 3) {
+			robot = new Robot();
+		}
+	}
+
+	public void goToXY() {
+		p = new PlayerPose2d();
+		System.out.println("Moving to co ordinates...");
+		p.setPx(x);
+		p.setPy(y);
+		robot.pos2D.setPosition(p, new PlayerPose2d(), 1);
+	}
+
+	public void initThread() {
+		OdometryThread oThread = new OdometryThread(robot.pos2D);
+		oThread.setRunning(true);
+		oThread.start();
+		OAThread oaThread = new OAThread(robot.pos2D, robot.sonar, x, y);
+		oaThread.setRunning(true);
+		oaThread.start();
+	}
+
+	public void printFinal() {
+		System.out.print("FINAL>>>\nX: " + robot.pos2D.getX() + "\nY: "
+				+ robot.pos2D.getY());
+	}
+
+	// OBSTACLE AVOIDANCE THREAD
+	public class OAThread extends Thread {
+		private Position2DInterface pos2D;
+		private RangerInterface sonar;
+		private double[] sonarV;
+		private double threshold = 0.7;
+		private double sThreshold = threshold - 0.4;
+		double x, y;
+		private boolean running;
+
+		public void setRunning(boolean running) {
+			this.running = running;
+		}
+
+		public OAThread(Position2DInterface pos2D, RangerInterface sonar,
+				double x, double y) {
+			super();
+			this.pos2D = pos2D;
+			this.sonar = sonar;
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public void run() {
+			while (running) {
+				while (!sonar.isDataReady())
+					;
+				sonarV = sonar.getData().getRanges();
+				if (isClose(sonarV)) {
+					switch (checkDirection(sonarV)) {
+					case 0:
+						turnA(1);
+						break;
+					case 1:
+						turnA(-1);
+						break;
+					case 2:
+						turnB(1);
+						break;
+					case 3:
+						turnB(-1);
+						break;
+					case 4:
+						System.out.println("This shouldn't happen");
+					}
+					goToXY();
+				}
+				try {
+					sleep(200);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+
+		public void goToXY() {
+			p = new PlayerPose2d();
+			System.out.println("Moving to co ordinates...");
+			p.setPx(x);
+			p.setPy(y);
+			robot.pos2D.setPosition(p, new PlayerPose2d(), 1);
+		}
+
+		public boolean isClose(double[] sonarValues) {
+			if ((sonarValues[0] < threshold) || (sonarValues[1] < threshold)
+					|| (sonarValues[2] < sThreshold)
+					|| (sonarValues[3] < sThreshold)
+					|| (sonarValues[6] < sThreshold)
+					|| (sonarValues[7] < sThreshold)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public int checkDirection(double[] sonarValues) {
+			if ((sonarValues[0] < threshold) || (sonarValues[1] < threshold)) {
+				double fLeft = sonarValues[1];
+				double fRight = sonarValues[0];
+				if (fLeft > fRight) {
+					return 0;
+				} else {
+					return 1;
+				}
+			} else if ((sonarValues[2] < sThreshold)
+					|| (sonarValues[3] < sThreshold)) {
+				return 3;
+			} else if ((sonarValues[6] < sThreshold)
+					|| (sonarValues[7] < sThreshold)) {
+				return 2;
+			} else
+				return 4;
+		}
+
+		public void turnA(int direction) {
+			System.out.println("TurnA :" + direction);
+			int a;
+			if (direction == 1) {
+				a = 6;
+			} else {
+				a = 2;
+			}
+			pos2D.setSpeed(0, 0.5 * direction);
+			try {
+				sleep(3142);
+			} catch (InterruptedException e) {
+			}
+			pos2D.setSpeed(1, 0);
+			while (true) {
+				if (sonar.isDataReady()) {
+					sonarV = sonar.getData().getRanges();
+				}
+				if (isClear(sonarV[a], sonarV[a + 1], sonarV)) {
+					break;
+				}
+			}
+			pos2D.setSpeed(1, 0);
+			try {
+				sleep(500);
+			} catch (InterruptedException e) {
+			}
+			// pos2D.setSpeed(0, -0.5 * direction); try { sleep(3142); } catch
+			// (InterruptedException e) {}
+		}
+
+		public void turnB(int direction) {
+			System.out.println("TurnB :" + direction);
+			pos2D.setSpeed(0, 0.5 * direction);
+			try {
+				sleep(3142);
+			} catch (InterruptedException e) {
+			}
+			pos2D.setSpeed(1, 0);
+			try {
+				sleep(800);
+			} catch (InterruptedException e) {
+			}
+		}
+
+		public boolean isClear(double one, double two, double[] sonarV) {
+			if ((one > (threshold + 1) && two > (threshold + 1))
+					|| (isClose(sonarV))) {
+				System.out.println("Sensors clear:\n1. " + one + "\n2. " + two);
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	// ODOMETRY THREAD
+	public class OdometryThread extends Thread {
+
+		private Position2DInterface pos2D;
+		private boolean running;
+
+		public void setRunning(boolean running) {
+			this.running = running;
+		}
+
+		public OdometryThread(Position2DInterface pos2D) {
+			super();
+			this.pos2D = pos2D;
+		}
+
+		@Override
+		public void run() {
+			while (running) {
+				if (checkGoal()) {
+					System.out
+							.println("Goal has been reached...\nClosing in 10 seconds");
+					try {
+						sleep(5000);
+					} catch (InterruptedException e) {
+					}
+					System.out.println("X: " + pos2D.getX());
+					System.out.println("Y: " + pos2D.getY());
+					try {
+						sleep(5000);
+					} catch (InterruptedException e) {
+					}
+					System.exit(0);
+				}
+				try {
+					sleep(6000);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+
+		public boolean checkGoal() {
+			double currX, currY;
+			if (pos2D.isDataReady()) {
+				currX = pos2D.getX();
+				currY = pos2D.getY();
+				if ((currX > x - 0.1) && (currX < x + 0.1) && (currY > y - 0.1)
+						&& (currY < y + 0.1)) {
+					return true;
+				}
+			} else {
+				return false;
+			}
+			return false;
+		}
+	}
+
+	public class Robot {
+
+		Position2DInterface pos2D = null;
+		RangerInterface sonar = null;
+		PlayerClient robot = null;
+
+		public Robot() {
+			try {
+				robot = new PlayerClient("localhost", 6665);
+				pos2D = robot.requestInterfacePosition2D(0,
+						PlayerConstants.PLAYER_OPEN_MODE);
+				sonar = robot.requestInterfaceRanger(0,
+						PlayerConstants.PLAYER_OPEN_MODE);
+			} catch (PlayerException e) {
+				System.err.println("Robot: Error connecting to Player!\n>>>"
+						+ e.toString());
+				System.exit(1);
+			}
+			robot.runThreaded(-1, -1);
+		}
+
+		public void stop() {
+			pos2D.setSpeed(0, 0);
+		}
+	}
+
+	public static void main(String[] args) {
+		new MainApp2(args);
+	}
+
+}
